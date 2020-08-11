e6a7fff0b6b2e03e04f6ddf81f10cb2e5d367b6e
Kirill Ilin <Systemtm32@yandex.ru>
2020-08-11 15:53:59 +0300

    add: content with DB|propTypes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Header } from './component';
+import { Header, PizzaBlock } from './component';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" exact>
+          <Home items={pizzas} />
+        </Route>
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
